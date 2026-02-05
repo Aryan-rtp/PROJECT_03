d46002e12c3c1877a9e1ee066c1b3f7c14de51b8
rtpar <rtp.aryan@gmail.com>
2026-02-05 20:43:19 +0530

    Server Update

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config()
 const app = require("./src/app")
+
+
 const connectToDb = require("./src/db/db")
 connectToDb()
 
--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const authRouter = require("../src/routes/app.routes");
 const postRouter = require("./routes/post.routes");
 
+
+
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
